b775504ca0869f6f89b771c0e85505f700458fe4
xinyuezhu11 <judyxinyuezhu@gmail.com>
2022-11-09 19:30:25 +0800

    [xinyuezhu11]format: auto format by IDEA

--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -1,10 +1,12 @@
 package com.tw.cn.cap.gtb.todo;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
